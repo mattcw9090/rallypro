38ec050ee6eb2b747554e5f5f303fbad3e22c3c5
mattcw9090 <mattcw9090@gmail.com>
2025-07-28 15:33:23 +0800

    test

---
+++ RallyProMac/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  RallyProMac
+//
+//  Created by Matthew Chew on 28/7/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ RallyProMac/RallyProMacApp.swift
@@ -0,0 +1,32 @@
+//
+//  RallyProMacApp.swift
+//  RallyProMac
+//
+//  Created by Matthew Chew on 28/7/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RallyProMacApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
