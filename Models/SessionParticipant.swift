8fec7ffe2310df1218ff7607355397e52adbc8b5
mattcw9090 <mattcw9090@gmail.com>
2025-08-06 08:17:22 +0800

    test

--- Models/SessionParticipant.swift
+++ Models/SessionParticipant.swift
@@ -23,19 +23,23 @@ class SessionParticipant {
             if let rawValue = teamRawValue {
                 return Team(rawValue: rawValue)
             }
-            return nil // Unassigned team
+            return nil
         }
         set {
             teamRawValue = newValue?.rawValue
         }
     }
     
+    /// The position of the participant within their team (e.g., 0-based index)
+    var teamPosition: Int = -1
+
     var hasPaid: Bool = false
 
-    init(session: Session, player: Player, team: Team? = nil) {
+    init(session: Session, player: Player, team: Team? = nil, teamPosition: Int = -1) {
         self.session = session
         self.player = player
         self.teamRawValue = team?.rawValue
+        self.teamPosition = teamPosition
         self.compositeKey = "\(session.uniqueIdentifier)-\(player.id)"
     }
 }
